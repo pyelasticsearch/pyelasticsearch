65fa3f131f6daf26b868b1817bb062fe3c19df9c
Robert Eanes <github@robsinbox.com>
2010-03-13 00:11:20 -0500

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name = "pyelasticsearch",
+    version = "0.0.2",
+    description = "Lightweight python wrapper for elastic search.",
+    author = 'Robert Eanes',
+    author_email = 'python@robsinbox.com',
+    py_modules = ['pyelasticsearch'],
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
+    ],
+    url = 'http://github.com/rhec/pyelasticsearch'
+)
