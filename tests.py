6accfcebd9b2bf18cf8d05f1d4934daab0c0ff8b
Robert Eanes <github@robsinbox.com>
2010-05-06 00:52:37 -0400

    Unit tests to replace doctests

---
+++ tests.py
@@ -0,0 +1,120 @@
+import unittest
+from pyelasticsearch import ElasticSearch
+import logging
+logging.basicConfig(filename="test.log", level=logging.DEBUG)
+
+class ElasticSearchTestCase(unittest.TestCase):
+    def setUp(self):
+        self.conn = ElasticSearch('http://localhost:9200/')
+        
+    def tearDown(self):
+        self.conn.delete_index("test-index")
+
+    def assertResultContains(self, result, expected):
+        for (key, value) in expected.items():
+            self.assertEquals(value, result[key])
+
+class IndexingTestCase(ElasticSearchTestCase):
+        
+    def testIndexingWithID(self):
+        result = self.conn.index({"name":"Joe Tester"}, "test-index", "test-type", 1)
+        self.assertResultContains(result, {'_type': 'test-type', '_id': '1', 'ok': True, '_index': 'test-index'} )
+
+    def testIndexingWithoutID(self):
+        result = self.conn.index({"name":"Joe Tester"}, "test-index", "test-type")
+        self.assertResultContains(result, {'_type': 'test-type', 'ok': True, '_index': 'test-index'} )
+        # should have an id of some value assigned.
+        self.assertTrue(result.has_key('_id') and result['_id'])
+        
+    def testExplicitIndexCreate(self):
+        result = self.conn.create_index("test-index")
+        self.assertResultContains(result, {'acknowledged': True, 'ok': True})
+    
+    def testDeleteByID(self):
+        self.conn.index({"name":"Joe Tester"}, "test-index", "test-type", 1)
+        self.conn.refresh(["test-index"])
+        result = self.conn.delete("test-index", "test-type", 1)
+        self.assertResultContains(result, {'_type': 'test-type', '_id': '1', 'ok': True, '_index': 'test-index'})
+        
+    def testDeleteIndex(self):
+        self.conn.create_index("another-index")
+        result = self.conn.delete_index("another-index")
+        self.assertResultContains(result, {'acknowledged': True, 'ok': True})
+        
+    def testCannotCreateExistingIndex(self):
+        self.conn.create_index("another-index")
+        result = self.conn.create_index("another-index")
+        self.conn.delete_index("another-index")
+        self.assertResultContains(result, {'error': '[another-index] Already exists'})
+
+    def testPutMapping(self):
+        result = self.conn.create_index("test-index")        
+        result = self.conn.put_mapping("test-type", {"test-type" : {"properties" : {"name" : {"type" : "string", "store" : "yes"}}}}, indexes=["test-index"])
+        self.assertResultContains(result, {'acknowledged': True, 'ok': True})
+        
+    def testIndexStatus(self):
+        self.conn.create_index("another-index")
+        result = self.conn.status(["another-index"])
+        self.conn.delete_index("another-index")
+        self.assertTrue(result.has_key('indices'))
+        self.assertResultContains(result, {'ok': True})
+        
+    def testIndexFlush(self):
+        self.conn.create_index("another-index")
+        result = self.conn.flush(["another-index"])
+        self.conn.delete_index("another-index")
+        self.assertResultContains(result, {'ok': True})
+        
+    def testIndexRefresh(self):
+        self.conn.create_index("another-index")
+        result = self.conn.refresh(["another-index"])
+        self.conn.delete_index("another-index")
+        self.assertResultContains(result, {'ok': True})
+
+    def testIndexOptimize(self):
+        self.conn.create_index("another-index")
+        result = self.conn.optimize(["another-index"])
+        self.conn.delete_index("another-index")
+        self.assertResultContains(result, {'ok': True})
+
+                
+class SearchTestCase(ElasticSearchTestCase):
+    def setUp(self):
+        super(SearchTestCase, self).setUp()
+        self.conn.index({"name":"Joe Tester"}, "test-index", "test-type", 1)
+        self.conn.index({"name":"Bill Baloney"}, "test-index", "test-type", 2)
+        self.conn.refresh(["test-index"])
+    
+    def testGetByID(self):
+        result = self.conn.get("test-index", "test-type", 1)
+        self.assertResultContains(result, {'_type': 'test-type', '_id': '1', '_source': {'name': 'Joe Tester'}, '_index': 'test-index'})
+
+    def testGetCountBySearch(self):
+        result = self.conn.count("name:joe")
+        self.assertResultContains(result, {'count': 1})
+
+    def testSearchByField(self):
+        result = self.conn.search("name:joe")
+        self.assertResultContains(result, {'hits': {'hits': [{'_type': 'test-type', '_id': '1', '_source': {'name': 'Joe Tester'}, '_index': 'test-index'}], 'total': 1}})
+
+    def testTermsByField(self):
+        result = self.conn.terms(['name'])
+        self.assertResultContains(result, {'docs': {'max_doc': 2, 'num_docs': 2, 'deleted_docs': 0}, 'fields': {'name': {'terms': [{'term': 'baloney', 'doc_freq': 1}, {'term': 'bill', 'doc_freq': 1}, {'term': 'joe', 'doc_freq': 1}, {'term': 'tester', 'doc_freq': 1}]}}})
+        
+    def testTermsByIndex(self):
+        result = self.conn.terms(['name'], indexes=['test-index'])
+        self.assertResultContains(result, {'docs': {'max_doc': 2, 'num_docs': 2, 'deleted_docs': 0}, 'fields': {'name': {'terms': [{'term': 'baloney', 'doc_freq': 1}, {'term': 'bill', 'doc_freq': 1}, {'term': 'joe', 'doc_freq': 1}, {'term': 'tester', 'doc_freq': 1}]}}})
+
+    def testTermsMinFreq(self):
+        result = self.conn.terms(['name'], min_freq=2)
+        self.assertResultContains(result, {'docs': {'max_doc': 2, 'num_docs': 2, 'deleted_docs': 0}, 'fields': {'name': {'terms': []}}})
+        
+    def testMLT(self):
+        self.conn.index({"name":"Joe Test"}, "test-index", "test-type", 3)
+        self.conn.refresh(["test-index"])
+        import time
+        result = self.conn.morelikethis("test-index", "test-type", 1, ['name'], min_term_frequency=1, min_doc_freq=1)
+        self.assertResultContains(result, {'hits': {'hits': [{'_type': 'test-type', '_id': '3', '_source': {'name': 'Joe Test'}, '_index': 'test-index'}], 'total': 1}})
+        
+if __name__ == "__main__":
+    unittest.main()
