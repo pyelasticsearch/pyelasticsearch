1152f9a1a8306bf6075c1f6d8c882e47c94ed2a8
Erik Rose <grinch@grinchcentral.com>
2012-09-20 14:20:27 -0700

    90% of the time, this is going to be used for servers, so let's come down from orbit and use a concrete term.

--- pyelasticsearch/downtime.py
+++ pyelasticsearch/downtime.py
@@ -7,39 +7,39 @@ from time import time
 
 class DowntimePronePool(object):
     """
-    A thread-safe bucket of things that may have downtime.
+    A thread-safe bucket of servers (or other things) that may have downtime.
 
-    Tries to return a "live" element from the bucket on request, retiring
-    "dead" elements for a time to give them a chance to recover before offering
-    them again.
+    Tries to return a "live" server from the bucket on request, retiring "dead"
+    servers for a time to give them a chance to recover before offering them
+    again.
 
-    Actually testing whether an element is dead is expressly outside the scope
+    Actually testing whether an server is dead is expressly outside the scope
     of this class (for decoupling) and outside the period of its lock (since it
     could take a long time). Thus, we explicitly embrace the race condition
-    where 2 threads are testing an element simultaneously, get different
+    where 2 threads are testing a server simultaneously, get different
     results, and call ``mark_dead`` and then ``mark_live`` fairly close
     together. It's not at all clear which is the correct state in that case, so
     we just let the winner win. If flapping is a common case, we could add flap
     detection later and class flappers as failures, immune to ``mark_live``.
     """
-    def __init__(self, elements, revival_delay):
-        self.live = elements
+    def __init__(self, servers, revival_delay):
+        self.live = servers
         self.dead = deque()  # [(time to reinstate, url), ...], oldest first
         self.revival_delay = revival_delay
         self.lock = Lock()  # a lock around live and dead
 
     def get(self):
         """
-        Return a random element and a bool indicating whether it was from the
+        Return a random server and a bool indicating whether it was from the
         dead list.
 
-        We prefer to return live servers. However, if all elements are marked
+        We prefer to return live servers. However, if all servers are marked
         dead, return one of those in case it's come back to life earlier than
         expected. This fallback is O(n) rather than O(1), but it's all dwarfed
         by IO anyway.
         """
         with self._locking():
-            # Revive any elements whose times have come:
+            # Revive any servers whose times have come:
             now = time()
             while self.dead and now >= self.dead[0][0]:
                 self.live.append(self.dead.popleft()[1])
@@ -49,39 +49,39 @@ class DowntimePronePool(object):
             except IndexError:  # live is empty.
                 return random.choice(self.dead)[1], True  # O(n) but rare
 
-    def mark_dead(self, element):
+    def mark_dead(self, server):
         """
-        Guarantee that this element won't be returned again until a period of
-        time has passed, unless all elements are dead.
+        Guarantee that this server won't be returned again until a period of
+        time has passed, unless all servers are dead.
 
-        If the given element is already on the dead list, do nothing. We
+        If the given server is already on the dead list, do nothing. We
         wouldn't want to push its revival time farther away.
         """
         with self._locking():
             try:
-                self.live.remove(element)
+                self.live.remove(server)
             except ValueError:
-                # Another thread has marked this element dead since this one
-                # got ahold of it, or we handed them a dead element to begin
+                # Another thread has marked this server dead since this one
+                # got ahold of it, or we handed them a dead server to begin
                 # with.
                 pass
             else:
-                self.dead.append((time() + self.revival_delay, element))
+                self.dead.append((time() + self.revival_delay, server))
 
-    def mark_live(self, element):
+    def mark_live(self, server):
         """
-        Move an element from the dead list to the live one.
+        Move a server from the dead list to the live one.
 
-        If the element wasn't dead, do nothing.
+        If the server wasn't dead, do nothing.
 
         This is intended to be used only in the case where ``get()`` falls back
-        to returning a dead element and we find out it isn't acting dead after
+        to returning a dead server and we find out it isn't acting dead after
         all.
         """
         with self._locking():
-            for i, (revival_time, cur_element) in enumerate(self.dead):
-                if cur_element == element:
-                    self.live.append(element)
+            for i, (revival_time, cur_server) in enumerate(self.dead):
+                if cur_server == server:
+                    self.live.append(server)
                     del self.dead[i]
                     break
             # If it isn't found, it's already been revived, and that's okay.
