513a485999325f2e61b48763d75ff24d2595cae7
Jannis Leidel <jannis@leidel.info>
2012-10-06 14:18:26 +0200

    Bumped to 0.2a1.

--- pyelasticsearch/__init__.py
+++ pyelasticsearch/__init__.py
@@ -20,7 +20,7 @@ from pyelasticsearch.exceptions import (Timeout, ConnectionError,
 __author__ = 'Robert Eanes'
 __all__ = ['ElasticSearch', 'ElasticHttpError', 'InvalidJsonResponseError',
            'Timeout', 'ConnectionError', 'ElasticHttpNotFoundError']
-__version__ = '0.3.1'
+__version__ = '0.2a1'
 __version_info__ = tuple(__version__.split('.'))
 
 get_version = lambda: __version_info__
