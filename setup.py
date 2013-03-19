ac5e09260c867056182723e9389113bc46a44510
Erik Rose <erik@mozilla.com>
2013-03-19 13:41:31 -0700

    Add Python 3 trove classifiers to setup.py.

--- setup.py
+++ setup.py
@@ -41,6 +41,8 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
         'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
     ],
     requires=[  # Needed?
