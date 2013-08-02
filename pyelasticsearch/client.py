71a7f4df1aa989f98d81d7ad84c59d93296b0c7e
Jannis Leidel <jannis@leidel.info>
2013-08-02 18:15:17 +0200

    Handle IndexAlreadyExistsException when nested in RemoteTransportException.
    This happens if one node of a cluster raises the exception instead of
    the one the client is connected to. The exception string looks like this
    for example:
    
      RemoteTransportException[[hostname][inet[/someip:1234]][indices/create]]; nested: IndexAlreadyExistsException[[main_index] Already exists];

--- pyelasticsearch/client.py
+++ pyelasticsearch/client.py
@@ -262,7 +262,8 @@ class ElasticSearch(object):
         error_class = ElasticHttpError
         if response.status_code == 404:
             error_class = ElasticHttpNotFoundError
-        elif error_message.startswith('IndexAlreadyExistsException'):
+        elif (error_message.startswith('IndexAlreadyExistsException') or
+              'nested: IndexAlreadyExistsException' in error_message):
             error_class = IndexAlreadyExistsError
 
         raise error_class(response.status_code, error_message)
