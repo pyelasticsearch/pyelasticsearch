be82718deab62d0376f3a1f1d68d06c385161134
Syrus Akbary <me@syrusakbary.com>
2013-08-08 23:53:50 +0200

    Added support for Thrift :)

--- pyelasticsearch/handlers.py
+++ pyelasticsearch/handlers.py
@@ -10,6 +10,15 @@ except ImportError:
     from urllib import urlencode, quote_plus
     from urlparse import urlparse
 
+try:
+    from thrift.transport import TTransport
+    from thrift.transport import TSocket
+    from thrift.protocol import TBinaryProtocol
+    from pyelasticsearch.pyelasticsearchthrift import Rest as pyesRest, ttypes as pyesTtypes
+    THRIFT_ENABLED = True
+except:
+    THRIFT_ENABLED = False
+
 from pyelasticsearch.utils import obj_to_utf8, obj_to_query
 from pyelasticsearch.exceptions import InvalidJsonResponseError
 
@@ -44,6 +53,30 @@ class RequestsHandler(object):
         resp = req_method(self.base+uri, timeout=self.timeout, **extra)
         return self._decode_response(resp), resp.status_code
 
+
+class ThriftHandler(object):
+    def __init__(self, host, port=9500, framed_transport=False, timeout=60, recycle=True):
+        socket = TSocket.TSocket(host, port)
+        if timeout is not None:
+            socket.setTimeout(timeout * 1000.0)
+        if framed_transport:
+            transport = TTransport.TFramedTransport(socket)
+        else:
+            transport = TTransport.TBufferedTransport(socket)
+        protocol = TBinaryProtocol.TBinaryProtocolAccelerated(transport)
+        self.client = pyesRest.Client(protocol)
+        transport.open()
+        # transport.close()
+
+    def request(self, method, uri, parameters=None, headers=None, body=None):
+        print uri
+        res = pyesTtypes.RestRequest(pyesTtypes.Method._NAMES_TO_VALUES[method.upper()], uri, parameters, {}, body)
+        response = self.client.execute(res)
+        return json.loads(response.body), response.status
+
+
+THRIFT_PORTS = [9500]
+
 def handler_from_url(url, **kwargs):
     parsed_url = urlparse(url)
     if url.find('://') < 0:
@@ -51,4 +84,6 @@ def handler_from_url(url, **kwargs):
     host = parsed_url.hostname
     port = parsed_url.port
     secure = parsed_url.scheme == 'https'
+    if THRIFT_ENABLED and port in THRIFT_PORTS:
+        return ThriftHandler(host=host, port=port)
     return RequestsHandler(host=host, port=port, secure=secure, **kwargs)
