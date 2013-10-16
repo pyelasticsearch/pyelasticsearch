3700a88b23a6a084fc5312d3725107b50ef65117
Natim <hubscher.remy@gmail.com>
2013-10-16 16:02:06 +0200

    requests 2.0 is still compatible

--- setup.py
+++ setup.py
@@ -55,11 +55,11 @@ setup(
     ],
     requires=[  # Needed?
         'six',
-        'requests(>=1.0,<2.0)',
+        'requests(>=1.0,<3.0)',
         'simplejson(>=2.1.0)',
     ],
     install_requires=[
-        'requests>=1.0,<2.0',
+        'requests>=1.0,<3.0',
         'simplejson>=2.1.0',
         'six'
     ],
