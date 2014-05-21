a18a19345298c43400dbfb984f97e97b3d0b624a
Erik Rose <erik@mozilla.com>
2014-05-21 11:24:09 -0400

    Change author and bump version.

--- pyelasticsearch/__init__.py
+++ pyelasticsearch/__init__.py
@@ -7,11 +7,11 @@ from pyelasticsearch.exceptions import (Timeout, ConnectionError,
                                         ElasticHttpNotFoundError,
                                         IndexAlreadyExistsError)
 
-__author__ = 'Robert Eanes'
+__author__ = 'Erik Rose'
 __all__ = ['ElasticSearch', 'ElasticHttpError', 'InvalidJsonResponseError',
            'Timeout', 'ConnectionError', 'ElasticHttpNotFoundError',
            'IndexAlreadyExistsError']
-__version__ = '0.6.1'
+__version__ = '0.7'
 __version_info__ = tuple(__version__.split('.'))
 
 get_version = lambda: __version_info__
