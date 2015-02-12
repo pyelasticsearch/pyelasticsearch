06dc49becd393e07086e368b26ab1aea3a9bc149
Erik Rose <erik@mozilla.com>
2015-02-12 13:53:58 -0500

    Add a wait to see if we can get Travis passing again.

--- pyelasticsearch/tests/__init__.py
+++ pyelasticsearch/tests/__init__.py
@@ -1,18 +1,32 @@
 """
 Unit tests for pyelasticsearch
 
-These require an elasticsearch server running on the default port
+These require a local elasticsearch server running on the default port
 (localhost:9200).
 """
-import unittest
+from time import sleep
+from unittest import TestCase
 
+from nose import SkipTest
 from nose.tools import eq_
+from six.moves import xrange
 
 # Test that __all__ is sufficient:
 from pyelasticsearch import *
 
 
-class ElasticSearchTestCase(unittest.TestCase):
+def setUp():
+    """When loading the test package, wait for ES to come up."""
+    for _ in xrange(200):
+        try:
+            ElasticSearch().health(wait_for_status='yellow')
+            return
+        except ConnectionError:
+            sleep(.1)
+    raise SkipTest('Could not connect to the ES server.')
+
+
+class ElasticSearchTestCase(TestCase):
     def setUp(self):
         self.conn = ElasticSearch()
 
